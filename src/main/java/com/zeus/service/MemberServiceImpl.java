4ab11319e1244c89780196072a8d876dad9de44f
domino3333 <busster@naver.com>
2026-01-30 12:23:05 +0900

    dml에 @Transactional 추가

--- src/main/java/com/zeus/service/MemberServiceImpl.java
+++ src/main/java/com/zeus/service/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zeus.domain.Member;
 import com.zeus.mapper.MemberMapper;
@@ -15,6 +16,7 @@ public class MemberServiceImpl implements MemberService {
 	private MemberMapper mapper;
 	
 	@Override
+	@Transactional
 	public int register(Member member) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
@@ -33,12 +35,14 @@ public class MemberServiceImpl implements MemberService {
 	}
 
 	@Override
+	@Transactional
 	public int update(Member member) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
+	@Transactional
 	public int delete(Member member) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
